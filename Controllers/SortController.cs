e7a59fa29d7c4759c67778dc9c2fc1904e168de1
Yena-Park <yena8317@gmail.com>
2018-11-11 01:18:51 -0500

    Fix JSON

--- Controllers/SortController.cs
+++ Controllers/SortController.cs
@@ -7,49 +7,69 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
 using mapsted.Models;
+using mapsted.Utils;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace mapsted.Controllers
 {
-    //[Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/")]
     public class SortController : Controller
     {
-        // GET: /<controller>/
-        public string Index()
+        [HttpPost("[action]")]
+        public JsonResult Sort([FromBody] SortItem sortItem)
         {
-            return "This is my default action...";
+            string[] splitValues = sortItem.InputValue.Split(',');
+            int[] intValues;
+
+            if (splitValues.Length == 0 || splitValues.Length > 500) {
+                return Json(new { success = false, error = "Input size is 1 to 500 values." });
+            }
+
+            switch(sortItem.InputTypeValue) {
+                case "int":
+                    if (!ValidateNumbers(splitValues))
+                        return Json(new { success = false, error = "Invalid Numbers(not integer size)" });
+
+                    intValues = Array.ConvertAll(splitValues, (string value) =>
+                    {
+                        return int.Parse(value);
+                    });
+
+                    return Json(new { success = true, data = InsertionSort.SortIntArray(intValues) });
+                case "string":
+                    if (!ValidateStrings(splitValues))
+                        return Json(new { success = false, error = "String values are up to 10 characters long." });
+
+                    return Json(new { success = true, data = InsertionSort.SortStringArray(splitValues) });
+                default:
+                    return Json(new { success = false, error = "Invalid input type." });
+            }
         }
 
-        // GET: /Sort/Welcome
-        public string Welcome()
+        private bool ValidateStrings(string[] values)
         {
-            return "This is the Welcome aiction method...";
+            foreach(string value in values) {
+                if (value.Length > 10 ) {
+                    return false;
+                }
+            }
+            return true; 
         }
 
-        public string Sort(string type, string value)
-        {
-            return HtmlEncoder.Default.Encode($"{type} {value}");
+        private bool ValidateNumbers(string[] values){
+            long num64 = new long();
+            foreach(string value in values) {
+                if(!(long.TryParse(value, out num64))) {
+                    return false;
+                } else if(num64 > int.MaxValue) {
+                    return false;
+                } else if(num64 < int.MinValue) {
+                    return false;
+                }
+            }
+            return true;
         }
-
-
-        public ActionResult<SortedResult> Sort2(string type, string value)
-        {
-            SortedResult result = new SortedResult();
-            result.Steps = new[] { "1,1,2,3,4,5,6", "1,1,2,3,4,5,6", "1,1,2,3,4,5,6" };
-
-            return result;
-        }
-
-        [HttpGet("{id}", Name = "GetTodo")]
-        public ActionResult<SortedResult> GetById(long id)
-        {
-            var item = new SortedResult();
-            item.Steps = new[] { "1,1,2,3,4,5,6", "1,1,2,3,4,5,6", "1,1,2,3,4,5,6" };
-
-
-            return item;
-        }
-
     }
 }
